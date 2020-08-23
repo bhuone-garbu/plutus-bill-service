ce3cecbd6578c622e83b431f12bdd8b9d21bbf3b
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2020-08-23 20:34:54 +0100

    implementation of path variable example in springboot 👍

--- src/main/java/com/garbu/plutus/api/ExpenseController.java
+++ src/main/java/com/garbu/plutus/api/ExpenseController.java
@@ -3,18 +3,34 @@ package com.garbu.plutus.api;
 import com.garbu.plutus.model.Expense;
 import com.garbu.plutus.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 public class ExpenseController {
 
+    private static List<Expense> expenses = new ArrayList<>();
+    private static User testUser = new User(UUID.randomUUID(), "John", "Doe",
+            "something@gmail.com", "07123456789");
+
+    static {
+        expenses.add(new Expense(UUID.randomUUID(), "this is a test", new BigDecimal("2.3"), testUser));
+    }
+
     @GetMapping("/expenses")
-    public Expense getExpense(@RequestParam(value = "id", required = false) String id) {
-        User testUser = new User(UUID.randomUUID(), "John", "Doe", "something@gmail.com", "07123456789");
-        return new Expense(UUID.randomUUID(), "this is a test", new BigDecimal("2.3"), testUser);
+    public List<Expense> getAll(@RequestParam(value = "amount", required = false) String amount) {
+        return expenses;
+    }
+
+    @GetMapping("expenses/{id}")
+    public Expense getOne(@PathVariable String id) {
+        // TODO: Service method to get an expense from the id
+        return expenses.get(0);
     }
 }
